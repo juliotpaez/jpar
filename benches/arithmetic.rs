ca5a7bcd3eba753417ac34154d3bf00048cc59af
Julio Treviño Páez <julio.tpaez@gmail.com>
2021-04-01 17:28:31 +0200

    Add some benches and integration tests

---
+++ benches/arithmetic.rs
@@ -0,0 +1,94 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+
+use parfet::parsers::branch::alternative;
+use parfet::parsers::characters::{
+    decimal_digit1, read_any_of, read_char, ucd_single_line_whitespace0,
+};
+use parfet::parsers::helpers::map_result;
+use parfet::parsers::sequence::{delimited, repeat_and_fold, tuple};
+use parfet::parsers::verifier::text_verifier;
+use parfet::result::ParserResult;
+use parfet::Reader;
+
+// Parser definition
+
+// We transform an integer string into a i64, ignoring surrounding whitespaces
+// We look for a digit suite, and try to convert it.
+// If there are no digits, we look for a parenthesized expression.
+fn factor(input: &mut Reader) -> ParserResult<i64> {
+    delimited(
+        ucd_single_line_whitespace0,
+        alternative((
+            map_result(decimal_digit1, |digits| {
+                unsafe { std::str::from_utf8_unchecked(digits.as_bytes()) }
+                    .parse()
+                    .unwrap()
+            }),
+            delimited(read_char('('), expr, read_char(')')),
+        )),
+        ucd_single_line_whitespace0,
+    )(input)
+}
+
+// We read an initial factor and for each time we find
+// a * or / operator followed by another factor, we do
+// the math by folding everything
+fn term(input: &mut Reader) -> ParserResult<i64> {
+    let init = factor(input)?;
+    repeat_and_fold(
+        ..,
+        init,
+        |acc, (op, val)| {
+            if op == '*' {
+                acc * val
+            } else {
+                acc / val
+            }
+        },
+        tuple((read_any_of(text_verifier("*/")), factor)),
+    )(input)
+}
+
+fn expr(input: &mut Reader) -> ParserResult<i64> {
+    let init = term(input)?;
+    repeat_and_fold(
+        ..,
+        init,
+        |acc, (op, val)| {
+            if op == '+' {
+                acc + val
+            } else {
+                acc - val
+            }
+        },
+        tuple((read_any_of(text_verifier("+-")), term)),
+    )(input)
+}
+
+fn arithmetic_bench(c: &mut Criterion) {
+    let data = "  2*2 / ( 5 - 1) + 3 / 4 * (2 - 7 + 567 *12 /2) + 3*(1+2*( 45 /2));";
+    let mut reader = Reader::new(data);
+
+    let result = expr(&mut reader);
+    assert_eq!(
+        result,
+        Ok(2 * 2 / (5 - 1) + 3 / 4 * (2 - 7 + 567 * 12 / 2) + 3 * (1 + 2 * (45 / 2)))
+    );
+
+    assert_eq!(reader.remaining_content(), ";");
+
+    c.bench_function("arithmetic", |b| {
+        let mut reader = Reader::new(data);
+        let init_cursor = reader.save_cursor();
+        b.iter(|| {
+            reader.restore(init_cursor.clone());
+            expr(&mut reader).unwrap()
+        });
+    });
+}
+
+criterion_group!(benches, arithmetic_bench);
+criterion_main!(benches);
---
+++ benches/json.rs
@@ -0,0 +1,191 @@
+#[macro_use]
+extern crate criterion;
+
+use std::collections::HashMap;
+
+use criterion::Criterion;
+
+use parfet::parsers::branch::alternative;
+use parfet::parsers::characters::{
+    read_any, read_any_quantified, read_char, read_none_of, read_text, ucd_whitespace0,
+};
+use parfet::parsers::combinator::verify;
+use parfet::parsers::helpers::{and_then, map_result};
+use parfet::parsers::numbers::read_float;
+use parfet::parsers::sequence::{
+    delimited, preceded, repeat_and_fold, repeat_separated, separated_tuple,
+};
+use parfet::parsers::verifier::text_verifier;
+use parfet::result::{ParserResult, ParserResultError};
+use parfet::Reader;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum JsonValue {
+    Null,
+    Bool(bool),
+    Str(String),
+    Num(f64),
+    Array(Vec<JsonValue>),
+    Object(HashMap<String, JsonValue>),
+}
+
+fn boolean(input: &mut Reader) -> ParserResult<bool> {
+    alternative((
+        map_result(read_text("false"), |_| false),
+        map_result(read_text("true"), |_| true),
+    ))(input)
+}
+
+fn u16_hex(input: &mut Reader) -> ParserResult<u16> {
+    map_result(read_any_quantified(4), |s| {
+        u16::from_str_radix(s, 16).unwrap()
+    })(input)
+}
+
+fn unicode_escape(input: &mut Reader) -> ParserResult<char> {
+    map_result(
+        alternative((
+            // Not a surrogate
+            map_result(verify(u16_hex, |cp| !(0xD800..0xE000).contains(cp)), |cp| {
+                cp as u32
+            }),
+            // See https://en.wikipedia.org/wiki/UTF-16#Code_points_from_U+010000_to_U+10FFFF for details
+            map_result(
+                verify(
+                    separated_tuple((u16_hex, u16_hex), read_text("\\u")),
+                    |(high, low)| (0xD800..0xDC00).contains(high) && (0xDC00..0xE000).contains(low),
+                ),
+                |(high, low)| {
+                    let high_ten = (high as u32) - 0xD800;
+                    let low_ten = (low as u32) - 0xDC00;
+                    (high_ten << 10) + low_ten + 0x10000
+                },
+            ),
+        )),
+        |x| std::char::from_u32(x).unwrap(),
+    )(input)
+}
+
+fn character(input: &mut Reader) -> ParserResult<char> {
+    let c = read_none_of(text_verifier("\""))(input)?;
+    if c == '\\' {
+        alternative((
+            and_then(read_any, |c| {
+                Ok(match c {
+                    '"' | '\\' | '/' => c,
+                    'b' => '\x08',
+                    'f' => '\x0C',
+                    'n' => '\n',
+                    'r' => '\r',
+                    't' => '\t',
+                    _ => return Err(ParserResultError::NotFound),
+                })
+            }),
+            preceded(read_char('u'), unicode_escape),
+        ))(input)
+    } else {
+        Ok(c)
+    }
+}
+
+fn string(input: &mut Reader) -> ParserResult<String> {
+    delimited(
+        read_char('"'),
+        repeat_and_fold(
+            ..,
+            String::new(),
+            |mut string, c| {
+                string.push(c);
+                string
+            },
+            character,
+        ),
+        read_char('"'),
+    )(input)
+}
+
+fn ws<'a, P, C, R>(content: P) -> impl FnMut(&mut Reader<'a, C>) -> ParserResult<R>
+where
+    P: FnMut(&mut Reader<'a, C>) -> ParserResult<R>,
+{
+    delimited(ucd_whitespace0, content, ucd_whitespace0)
+}
+
+fn array(input: &mut Reader) -> ParserResult<Vec<JsonValue>> {
+    delimited(
+        read_char('['),
+        ws(repeat_separated(.., json_value, ws(read_char(',')))),
+        read_char(']'),
+    )(input)
+}
+
+fn object(input: &mut Reader) -> ParserResult<HashMap<String, JsonValue>> {
+    map_result(
+        delimited(
+            read_char('{'),
+            ws(repeat_separated(
+                ..,
+                separated_tuple((string, json_value), ws(read_char(':'))),
+                ws(read_char(',')),
+            )),
+            read_char('}'),
+        ),
+        |key_values| key_values.into_iter().collect(),
+    )(input)
+}
+
+fn json_value(input: &mut Reader) -> ParserResult<JsonValue> {
+    use JsonValue::*;
+
+    alternative((
+        map_result(read_text("null"), |_| Null),
+        map_result(boolean, Bool),
+        map_result(string, Str),
+        map_result(read_float, |s| {
+            let double = s.parse().unwrap();
+            Num(double)
+        }),
+        map_result(array, Array),
+        map_result(object, Object),
+    ))(input)
+}
+
+fn json(input: &mut Reader) -> ParserResult<JsonValue> {
+    ws(json_value)(input)
+}
+
+fn json_bench(c: &mut Criterion) {
+    let data = "  { \"a\"\t: 42,
+  \"b\": [ \"x\", \"y\", 12 ,\"\\u2014\", \"\\uD83D\\uDE10\"] ,
+  \"c\": { \"hello\" : \"world\"
+  }
+  }  ";
+
+    c.bench_function("json", |b| {
+        b.iter(|| {
+            let mut reader = Reader::new(data);
+            json(&mut reader).unwrap()
+        });
+    });
+}
+
+fn read_integer_bench(c: &mut Criterion) {
+    c.bench_function("read_integer", |b| {
+        b.iter(|| {
+            let mut reader = Reader::new("-1.234E-12");
+            read_float(&mut reader).unwrap()
+        });
+    });
+}
+
+fn read_float_bench(c: &mut Criterion) {
+    c.bench_function("read_float", |b| {
+        b.iter(|| {
+            let mut reader = Reader::new("-1.234E-12");
+            read_float(&mut reader).unwrap()
+        });
+    });
+}
+
+criterion_group!(benches, json_bench, read_integer_bench, read_float_bench,);
+criterion_main!(benches);
