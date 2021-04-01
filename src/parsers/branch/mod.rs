60431264f22cfee0cdd2311235e33dcd1e6f4cd6
Julio Treviño Páez <julio.tpaez@gmail.com>
2021-04-01 03:22:27 +0200

    Make conditions of branches to be parsers

--- src/parsers/branch/mod.rs
+++ src/parsers/branch/mod.rs
@@ -1,37 +1,39 @@
 pub use alternative::*;
 
+use crate::parsers::helpers::not_found_restore;
 use crate::result::ParserResult;
 use crate::Reader;
 
 mod alternative;
 
-/// Executes the parser depending on a condition. Returns None when condition is false.
+/// Executes the `condition` parser that returns a bool and depending on that executes or not the `then` parser.
+/// Returns None when condition is false.
 pub fn branch_if<'a, C, R>(
-    mut condition: impl FnMut(&mut Reader<'a, C>) -> bool,
+    mut condition: impl FnMut(&mut Reader<'a, C>) -> ParserResult<bool>,
     mut then: impl FnMut(&mut Reader<'a, C>) -> ParserResult<R>,
 ) -> impl FnMut(&mut Reader<'a, C>) -> ParserResult<Option<R>> {
-    move |reader| {
-        if condition(reader) {
+    not_found_restore(move |reader| {
+        if condition(reader)? {
             Ok(Some(then(reader)?))
         } else {
             Ok(None)
         }
-    }
+    })
 }
 
-/// Executes `then` or `else_parser` depending on a condition.
+/// Executes the `condition` parser that returns a bool and depending on that executes either `then` or `else_parser` parsers.
 pub fn branch_if_else<'a, C, R>(
-    mut condition: impl FnMut(&mut Reader<'a, C>) -> bool,
+    mut condition: impl FnMut(&mut Reader<'a, C>) -> ParserResult<bool>,
     mut then: impl FnMut(&mut Reader<'a, C>) -> ParserResult<R>,
     mut else_parser: impl FnMut(&mut Reader<'a, C>) -> ParserResult<R>,
 ) -> impl FnMut(&mut Reader<'a, C>) -> ParserResult<(bool, R)> {
-    move |reader| {
-        if condition(reader) {
+    not_found_restore(move |reader| {
+        if condition(reader)? {
             Ok((true, then(reader)?))
         } else {
             Ok((false, else_parser(reader)?))
         }
-    }
+    })
 }
 
 // ----------------------------------------------------------------------------
@@ -41,31 +43,45 @@ pub fn branch_if_else<'a, C, R>(
 #[cfg(test)]
 mod test {
     use crate::parsers::characters::read_text;
+    use crate::result::ParserResultError;
 
     use super::*;
 
     #[test]
     fn test_branch_if() {
         let mut reader = Reader::new("This is a test");
-        let mut parser = branch_if(|_| true, read_text("This"));
+        let mut parser = branch_if(|_| Ok(true), read_text("This"));
         let result = parser(&mut reader);
         assert_eq!(result, Ok(Some("This")));
 
-        let mut parser = branch_if(|_| false, read_text("This"));
+        let mut parser = branch_if(|_| Ok(false), read_text("This"));
         let result = parser(&mut reader);
         assert_eq!(result, Ok(None));
+
+        let mut parser = branch_if(|_| Err(ParserResultError::NotFound), read_text("This"));
+        let result = parser(&mut reader);
+        assert_eq!(result, Err(ParserResultError::NotFound));
     }
 
     #[test]
     fn test_branch_if_else() {
         let mut reader = Reader::new("This is a test");
-        let mut parser = branch_if_else(|_| true, read_text("This"), read_text("Th"));
+        let mut parser = branch_if_else(|_| Ok(true), read_text("This"), read_text("Th"));
         let result = parser(&mut reader);
         assert_eq!(result, Ok((true, "This")));
 
         let mut reader = Reader::new("This is a test");
-        let mut parser = branch_if_else(|_| false, read_text("This"), read_text("Th"));
+        let mut parser = branch_if_else(|_| Ok(false), read_text("This"), read_text("Th"));
         let result = parser(&mut reader);
         assert_eq!(result, Ok((false, "Th")));
+
+        let mut reader = Reader::new("This is a test");
+        let mut parser = branch_if_else(
+            |_| Err(ParserResultError::NotFound),
+            read_text("This"),
+            read_text("Th"),
+        );
+        let result = parser(&mut reader);
+        assert_eq!(result, Err(ParserResultError::NotFound));
     }
 }
